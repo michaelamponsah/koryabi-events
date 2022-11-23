e45f2e1d0d867b37de164e4c9fe3b3aac96596bf
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-23 18:55:12 +0000

    Modify main.js
    Added js code to prevent page body from scrolling when nav menu is active

--- js/main.js
+++ js/main.js
@@ -4,15 +4,30 @@ import speakerCardBuilder from './speakerCardBuilder.js';
 
 const mobileNavMenu = document.querySelector('.mobile-slider-nav');
 const keynoteSpeakersSection = document.querySelector('.section-speakers--keynote-speakers');
+const body = document.querySelector('body');
 
 document.addEventListener('click', (e) => {
   const isMobileNavMenuButton = e.target.matches('[data-nav-menu-btn]');
+  const isNavLink = e.target.matches('[data-nav-link]');
 
   if (isMobileNavMenuButton) {
     mobileNavMenu.classList.toggle('mobile-slider-nav-active');
   }
+
+  if (isNavLink && mobileNavMenu.classList.contains('mobile-slider-nav-active')) {
+    mobileNavMenu.classList.remove('mobile-slider-nav-active');
+    body.style.overflow = 'auto';
+  }
+
+  if (mobileNavMenu.classList.contains('mobile-slider-nav-active')) {
+    body.style.overflow = 'hidden';
+  } else {
+    body.style.overflow = 'auto';
+  }
 });
 
 keyNoteSpeakers.forEach((speaker) => {
-  keynoteSpeakersSection.insertAdjacentHTML('afterbegin', speakerCardBuilder(speaker));
+  if (keynoteSpeakersSection) {
+    keynoteSpeakersSection.insertAdjacentHTML('afterbegin', speakerCardBuilder(speaker));
+  }
 });

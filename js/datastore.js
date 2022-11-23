1e051365510f5bfa4acdb8126ea72fc1b809fe7b
michaelamponsah <amponsahmichaelkwame@gmail.com>
2022-11-23 15:35:28 +0000

    Add array of speaker objects in datastore.js

---
+++ js/datastore.js
@@ -0,0 +1,59 @@
+/* eslint-disable linebreak-style */
+const keyNoteSpeakers = [
+  {
+    id: 'speaker-1',
+    position: 'Prof. Friedman Lexy',
+    briefProfile: 'Senior Lecturer, Dept. of Geology. University of Ghana',
+    img: {
+      src: './assets/images/speaker-1.png',
+      alt: 'Headshot of Prof. Friedman Lexy',
+    },
+  },
+  {
+    id: 'speaker-2',
+    position: 'Dr. Daniel Butchel',
+    briefProfile: 'Senior Lecturer, Dept. of Geology. University of Alden',
+    img: {
+      src: './assets/images/speaker-2.png',
+      alt: 'Headshot of Dr. Daniel Butchel',
+    },
+  },
+  {
+    id: 'speaker-3',
+    position: 'Prof. Alex Costa',
+    briefProfile: 'Director of Center for Climate Change, University of Ghana',
+    img: {
+      src: './assets/images/speaker-3.png',
+      alt: 'Headshot of Prof. Alex Costa',
+    },
+  },
+  {
+    id: 'speaker-4',
+    position: 'Prof. Alex Costa',
+    briefProfile: 'Director of Center for Climate Change, University of Ghana',
+    img: {
+      src: './assets/images/speaker-3.png',
+      alt: 'Headshot of Prof. Alex Costa',
+    },
+  },
+  {
+    id: 'speaker-5',
+    position: 'Prof. Alex Costa',
+    briefProfile: 'Director of Center for Climate Change, University of Ghana',
+    img: {
+      src: './assets/images/speaker-3.png',
+      alt: 'Headshot of Prof. Alex Costa',
+    },
+  },
+  {
+    id: 'speaker-6',
+    position: 'Prof. Alex Costa',
+    briefProfile: 'Director of Center for Climate Change, University of Ghana',
+    img: {
+      src: './assets/images/speaker-3.png',
+      alt: 'Headshot of Prof. Alex Costa',
+    },
+  },
+];
+
+export default keyNoteSpeakers;
